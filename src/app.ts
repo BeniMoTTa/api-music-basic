83f2cc90a1cfd06c89e8d8717a871cc46d529834
Jozhué Beni <jbeeniif@gmail.com>
2023-10-18 10:23:20 -0300

    logics upload Audio

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,50 @@
-import express, { Application } from "express";
+import express, { Application, request } from "express";
+import { handleErrors } from "./errors/errors";
 import cors from "cors";
+import { PrismaClient } from "@prisma/client";
+const multer = require("multer");
+const path = require("path");
 
 export const app: Application = express();
+const audioStorage = multer.memoryStorage();
+const audioUpload = multer({ storage: audioStorage });
+
+const profileImageStorage = multer.diskStorage({
+  destination: function (
+    req: any,
+    file: any,
+    cb: (arg0: null, arg1: string) => void
+  ) {
+    cb(null, "profileImages/");
+  },
+  filename: function (
+    req: any,
+    file: { originalname: number },
+    cb: (arg0: null, arg1: any) => void
+  ) {
+    cb(null, Date.now() + file.originalname);
+  },
+});
+const profileImageUpload = multer({ storage: profileImageStorage });
+
+app.post("/uploadAudio", audioUpload.single("audio"), async (req, res) => {
+    try{
+        const {title} = req.body;
+        const audioData = req.filter;
+
+        const music = await PrismaClient.music.create({
+            data:{
+                title:title,
+                audioUrl:""
+            }
+        })
+    }
+
+
+
+});
 
 app.use(express.json());
 app.use(cors());
+
+app.use(handleErrors);
