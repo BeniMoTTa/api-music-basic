80d59b964d6e470cbf33cc9405574dfd1efecc21
Jozhué Beni <jbeeniif@gmail.com>
2023-10-19 10:41:32 -0300

    feat: retrieveUserService are on

---
+++ src/services/user/retrieveUsers.service.ts
@@ -0,0 +1,15 @@
+import { Users } from "@prisma/client";
+import { TUserResponse } from "../../interfaces/user.interfaces";
+import { prisma } from "../../server";
+import { userSchemaResponse } from "../../schemas/user.schema";
+
+export const retrieveUserService = async (
+  userId: number
+): Promise<TUserResponse> => {
+  const user: Users | null = await prisma.users.findFirst({
+    where: {
+      id: userId,
+    },
+  });
+  return userSchemaResponse.parse(user);
+};
