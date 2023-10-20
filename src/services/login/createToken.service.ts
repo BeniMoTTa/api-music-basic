2f6cb36331e861ffd4876b69c3f11c68dcbad2b5
Jozhué Beni <jbeeniif@gmail.com>
2023-10-20 11:03:18 -0300

    Create Token with jwt on Login

--- src/services/login/createToken.service.ts
+++ src/services/login/createToken.service.ts
@@ -15,4 +15,10 @@ export const createTokenService = async ({
   if (!user) {
     throw new AppError("Invalid credentials", 403);
   }
+
+  const token: string = jwt.sign(
+    { isSeller: user.is_seller },
+    process.env.SECRET_KEY!,
+    { expiresIn: process.env.EXPIRES_IN, subject: user.id.toString() }
+  );
 };
