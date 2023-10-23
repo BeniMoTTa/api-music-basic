5a15c4826620dcbb584f87fba4d67c011ed8b456
Jozhué Beni <jbeeniif@gmail.com>
2023-10-23 14:00:16 -0300

    vinculate deleteUser with controller

--- src/controllers/delete.controller.ts
+++ src/controllers/delete.controller.ts
@@ -1,6 +1,10 @@
 import { Request, Response } from "express";
+import { deleteUserService } from "../services/user/delete.service";
 
 export const deleteUserController = async (
   req: Request,
   res: Response
-): Promise<Response> => {};
+): Promise<Response> => {
+  const userId: number = +req.params.id;
+  await deleteUserService(userId);
+};
