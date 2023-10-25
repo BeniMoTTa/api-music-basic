9c961522f2d7544f9a38fb3ddc55a66d3f0dab26
Jozhué Beni <jbeeniif@gmail.com>
2023-10-25 15:11:02 -0300

    Create linear call with Service

--- src/controllers/user/listen.controller.ts
+++ src/controllers/user/listen.controller.ts
@@ -1,8 +1,12 @@
 import { Request, Response } from "express";
+import { TUserResponse } from "../../interfaces/user.interfaces";
+import { retrieveUserService } from "../../services/user/retrieveUsers.service";
 
 export const retrieveUserController = async (
   req: Request,
   res: Response
 ): Promise<Response> => {
   const userId: number = +req.params.id;
+
+  const user: TUserResponse = await retrieveUserService(userId);
 };
