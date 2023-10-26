cd447a91168801abcc8ac308e8232e452d1fb29e
Jozhué Beni <jbeeniif@gmail.com>
2023-10-26 14:32:31 -0300

    Value of body request

--- src/controllers/user/update.controller.ts
+++ src/controllers/user/update.controller.ts
@@ -1,8 +1,10 @@
 import { Request, Response } from "express";
+import { TUserUpdateRequest } from "../../interfaces/user.interfaces";
 
 export const updateUserController = async (
   req: Request,
   req: Response
 ): Promise<Response> => {
   const userId: number = +req.params.id;
+  const updatedValues: TUserUpdateRequest = req.body;
 };
