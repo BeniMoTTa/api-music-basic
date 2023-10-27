aaf86d2890487df57a4765fe86eadca99245398a
Jozhué Beni <jbeeniif@gmail.com>
2023-10-27 09:29:29 -0300

    Callback forgotPasswordService on COntrolle

--- src/controllers/user/forgotPassword.controller.ts
+++ src/controllers/user/forgotPassword.controller.ts
@@ -1,8 +1,10 @@
 import { Request, Response } from "express";
+import { forgotPasswordService } from "../../services/user/forgotPassword.service";
 
 export const forgotPasswordController = async (
   req: Request,
   res: Response
 ): Promise<Response> => {
   const { email } = req.body;
+  await forgotPasswordService(email);
 };
