5712304b74ee60f08407117cdc825fbbc5892930
Jozhué Beni <jbeeniif@gmail.com>
2023-11-01 14:09:59 -0300

    Feat: Create router for create user

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,3 +1,6 @@
 import { Router } from "express";
+import { createUserController } from "../controllers/user/create.controller";
 
 export const userRouter: Router = Router();
+
+userRouter.post("", createUserController);
