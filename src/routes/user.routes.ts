763ea533c402b7f95b108e59b00468f8e82cd9c1
Jozhué Beni <jbeeniif@gmail.com>
2023-11-01 14:12:25 -0300

    feat: create Delete user

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import { createUserController } from "../controllers/user/create.controller";
 import { retrieveUserController } from "../controllers/user/listen.controller";
+import { deleteUserController } from "../controllers/user/delete.controller";
 
 export const userRouter: Router = Router();
 
 userRouter.post("", createUserController);
 userRouter.get("/:id", retrieveUserController);
+userRouter.delete("/:id", deleteUserController);
