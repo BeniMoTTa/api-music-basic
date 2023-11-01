132aff1ce7b450a0dc30b1c8233fa70eb049ab2f
Jozhué Beni <jbeeniif@gmail.com>
2023-11-01 14:11:25 -0300

    feat: create Retrieve user to listen by id

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,6 +1,8 @@
 import { Router } from "express";
 import { createUserController } from "../controllers/user/create.controller";
+import { retrieveUserController } from "../controllers/user/listen.controller";
 
 export const userRouter: Router = Router();
 
 userRouter.post("", createUserController);
+userRouter.get("/:id", retrieveUserController);
