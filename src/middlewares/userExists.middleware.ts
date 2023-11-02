be59c4d715dfc354f3e82d52aaceb4311f0ea0a3
Jozhué Beni <jbeeniif@gmail.com>
2023-11-02 09:41:34 -0300

    Feat: Response on middleware

--- src/middlewares/userExists.middleware.ts
+++ src/middlewares/userExists.middleware.ts
@@ -1,3 +1,5 @@
 import { NextFunction, Response } from "express";
 
-export const userExistsMiddleware = (req:Request res:Response next:NextFunction);
+export const userExistsMiddleware = (req:Request res:Response next:NextFunction): <Response> => {
+
+};
