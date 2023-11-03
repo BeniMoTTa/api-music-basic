deacadf228355815adad505a5d76c420c2d3bb43
Jozhué Beni <jbeeniif@gmail.com>
2023-11-03 09:12:13 -0300

    fix: bug on id user

--- src/middlewares/userExists.middleware.ts
+++ src/middlewares/userExists.middleware.ts
@@ -1,7 +1,7 @@
 import { NextFunction, Response, response } from "express";
 
 export const userExistsMiddleware = (req:Request res:Response next:NextFunction): <Response> => {
-    const userId = req.id
+
 
 
     return response
