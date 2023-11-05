86bd6d9dc8e5d4178f84609bb617d2aad26cd54f
Jozhué Beni <jbeeniif@gmail.com>
2023-11-05 14:37:50 -0300

    fix: fixed bugs on comment

--- src/middlewares/userExists.middleware.ts
+++ src/middlewares/userExists.middleware.ts
@@ -2,7 +2,7 @@ import { NextFunction, Response, response } from "express";
 
 export const userExistsMiddleware = (req:Request res:Response next:NextFunction): <Response> => {
 
-// Como ass
+
 
     return response
 };
